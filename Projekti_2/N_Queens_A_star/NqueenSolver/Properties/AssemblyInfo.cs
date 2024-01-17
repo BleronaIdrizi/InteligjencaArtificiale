5b731bc1899acaea08382a0f38f15138c07579ac
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2024-01-17 22:06:34 +0100

    N-Queens Problem - Added methods and files project

---
+++ Projekti_2/N_Queens_A_star/NqueenSolver/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NqueenSolver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NqueenSolver")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11693b7e-410b-43e8-84be-1b430afd2a3d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
